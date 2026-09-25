48ba01fcf0cbe895d67cbbf4a9648ca8a79f95d8
FRY Developer <alexiusandromedavsgalaxia@gmail.com>
2026-09-25 16:14:59 +0200

    redesign dumbNavigator and fix runtime/storage issues

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  plugins:[react()],
-  server:{
-    headers:{
-      'Cross-Origin-Embedder-Policy':'require-corp',
-      'Cross-Origin-Opener-Policy':'same-origin'
-    }
-  }
+ plugins:[react()],
+ server:{headers:{'Cross-Origin-Embedder-Policy':'require-corp','Cross-Origin-Opener-Policy':'same-origin'}},
+ preview:{headers:{'Cross-Origin-Embedder-Policy':'require-corp','Cross-Origin-Opener-Policy':'same-origin'}}
 })
